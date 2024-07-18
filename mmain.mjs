dbb077c2cd19ae9013f1dd5c276b88e9d300074c
philin135 <42301221+philin135@users.noreply.github.com>
2024-07-18 16:49:44 +0800

    Add files via upload

---
+++ mmain.mjs
@@ -0,0 +1,11 @@
+// main.js
+
+// Importing the variable, function, and class from module.js
+import { myVariable, myFunction} from './mmodule.mjs';
+
+// Using the imported variable
+console.log(myVariable); // Output: Hello, World!
+
+// Using the imported function
+myFunction(); // Output: This is a function from module.js
+
---
+++ mmodule.mjs
@@ -0,0 +1,10 @@
+// module.js
+
+// Exporting a variable
+export const myVariable = "Hello, World!";
+
+// Exporting a function
+export function myFunction() {
+  console.log("This is a function from module.js");
+}
+
